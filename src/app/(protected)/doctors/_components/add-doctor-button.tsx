900892e631ec180a37f3e2750d0f9e461bd2ee02
ddouglss <ddouglss1999@gmail.com>
2025-08-09 03:07:08 -0300

    chore: atualizando o componente médico

--- src/app/(protected)/doctors/_components/add-doctor-button.tsx
+++ src/app/(protected)/doctors/_components/add-doctor-button.tsx
@@ -1,25 +1,26 @@
-'use client'
+"use client";
 
-import {Plus} from "lucide-react";
-import {useState} from "react";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 
-import UpsertDoctorForm from "@/app/(protected)/doctors/_components/upsert-doctor-form";
-import {Button} from "@/components/ui/button";
-import {Dialog, DialogTrigger} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+
+import UpsertDoctorForm from "./upsert-doctor-form";
 
 const AddDoctorButton = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <Dialog open={isOpen} onOpenChange={setIsOpen}>
-            <DialogTrigger asChild>
-                <Button>
-                    <Plus/>
-                    Adicionar médicos
-                </Button>
-            </DialogTrigger>
-            <UpsertDoctorForm onSuccess={() => setIsOpen(false)} />
-        </Dialog>
-    );
-}
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus />
+          Adicionar médico
+        </Button>
+      </DialogTrigger>
+      <UpsertDoctorForm onSuccess={() => setIsOpen(false)} isOpen={isOpen} />
+    </Dialog>
+  );
+};
 
 export default AddDoctorButton;
